dfb41ba9759da6ce4202bfa62be0d99e0dab6de3
Andrew Green <afgreen@cern.ch>
2015-07-13 12:34:32 +0100

    Change path to something more sensible

--- initial-cache.cmake
+++ initial-cache.cmake
@@ -6,7 +6,7 @@ set(CMAKE_CXX_COMPILER "g++-4.9" CACHE STRING "")
 set(CMAKE_C_COMPILER "gcc-4.9" CACHE STRING "")
 
 # Obviously change this path to the location you want Geant4 installed to
-set(CMAKE_INSTALL_PREFIX "/Users/agreen/howbig" CACHE PATH "")
+set(CMAKE_INSTALL_PREFIX "/Users/agreen/local" CACHE PATH "")
 
 # Select whether to build with multithreading support, and wether to install data
 set(GEANT4_BUILD_MULTITHREADED ON CACHE BOOL "")
