4301655900f4fdd37579dba6bd9e2ec6336197af
Andrew Green <afgreen@cern.ch>
2015-07-19 17:12:40 +0100

    Add visualisation options

--- initial-cache.cmake
+++ initial-cache.cmake
@@ -11,3 +11,7 @@ set(CMAKE_INSTALL_PREFIX "/Users/agreen/local" CACHE PATH "")
 # Select whether to build with multithreading support, and wether to install data
 set(GEANT4_BUILD_MULTITHREADED ON CACHE BOOL "")
 set(GEANT4_INSTALL_DATA ON CACHE BOOL "")
+
+# If you would like visualisation, set one of these to ON. I have selected Qt.
+set(GEANT4_USE_QT ON CACHE BOOL "")
+set(GEANT4_USE_OPENGL_X11 OFF CACHE BOOL "")
