b2af43ca181ec890d6cd78f240c86411f09e1bc4
Andrew Green <afgreen@cern.ch>
2015-10-16 11:49:42 +0100

    Updated options and made more general

--- initial-cache.cmake
+++ initial-cache.cmake
@@ -2,16 +2,22 @@
 set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
 
 # These may need to be changed if you have different compilers
-set(CMAKE_CXX_COMPILER "g++-4.9" CACHE STRING "")
-set(CMAKE_C_COMPILER "gcc-4.9" CACHE STRING "")
+set(CMAKE_CXX_COMPILER "g++" CACHE STRING "")
+set(CMAKE_C_COMPILER "gcc" CACHE STRING "")
 
 # Obviously change this path to the location you want Geant4 installed to
-set(CMAKE_INSTALL_PREFIX "/Users/agreen/local" CACHE PATH "")
+set(CMAKE_INSTALL_PREFIX "/usr/local/" CACHE PATH "")
 
 # Select whether to build with multithreading support, and wether to install data
 set(GEANT4_BUILD_MULTITHREADED ON CACHE BOOL "")
 set(GEANT4_INSTALL_DATA ON CACHE BOOL "")
 
 # If you would like visualisation, set one of these to ON. I have selected Qt.
-set(GEANT4_USE_QT ON CACHE BOOL "")
+set(GEANT4_USE_QT OFF CACHE BOOL "")
 set(GEANT4_USE_OPENGL_X11 OFF CACHE BOOL "")
+
+# Other Geant4 options...
+set(GEANT4_USE_GDML ON CACHE BOOL "")
+set(GEANT4_BUILD_STORE_TRAJECTORY OFF CACHE BOOL "")
+set(GEANT4_BUILD_VERBOSE_CODE OFF CACHE BOOL "")
+
